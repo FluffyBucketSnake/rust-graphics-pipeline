21f35dd858c1cc920b97bbf45c8d13c373f6dd84
Bruno Henrique Glowaksi Morais <bruno.glowaski@outlook.com>
2021-02-17 06:12:55 -0400

    Add draw_line function

---
+++ src/pipeline.rs
@@ -0,0 +1,19 @@
+use crate::framework::BitmapOutput;
+use crate::vec::Vec2f;
+use sdl2::pixels::Color;
+
+pub fn draw_line(target: &mut BitmapOutput, start: Vec2f, end: Vec2f) {
+    // Based on DDA algorithm.
+    let delta = end - start;
+    let step = f32::max(delta.x.abs(), delta.y.abs());
+    let Vec2f { x: xi, y: yi } = delta / step;
+
+    let Vec2f { mut x, mut y } = start;
+    let mut i: f32 = 1.0;
+    while i <= step {
+        target.put_pixel(x as i32, y as i32, Color::WHITE);
+        x += xi;
+        y += yi;
+        i += 1.0;
+    }
+}
