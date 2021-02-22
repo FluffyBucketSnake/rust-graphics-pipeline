66f40de85736ba896fb5e726d878954385f8e938
Bruno Henrique Glowaksi Morais <bruno.glowaski@outlook.com>
2021-02-22 05:04:22 -0400

    Simplify build_line_circle()

--- src/models.rs
+++ src/models.rs
@@ -2,12 +2,12 @@ use crate::math::{Vec2f, Vec3f};
 use crate::vertex::Vertex;
 use sdl2::pixels::Color;
 
-pub fn build_line_circle(origin: Vec3f, length: f32, dtheta: f32, colors: &(Color, Color)) -> Vec<(Vertex, Vertex)> {
+pub fn build_line_circle(dtheta: f32, colors: &(Color, Color)) -> Vec<(Vertex, Vertex)> {
     let mut result = Vec::new();
     let mut theta = 0.0f32;
     while theta < 2.0 * std::f32::consts::PI {
-        result.push((Vertex::new(origin, colors.0),
-                     Vertex::new(origin + (length * Vec3f::from_vec2(Vec2f::from_direction(theta), 1.0)), colors.1)));
+        result.push((Vertex::new(Vec3f::zero(), colors.0),
+                     Vertex::new(Vec3f::from_vec2(Vec2f::from_direction(theta), 1.0), colors.1)));
         theta += dtheta;
     }
     result
