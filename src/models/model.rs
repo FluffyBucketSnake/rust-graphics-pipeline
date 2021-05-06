9530a8836e87a335f5c862695a512cc96a2ebb80
FluffyBucketSnake <fluffybucketsnake@outlook.com>
2021-05-06 04:37:39 -0400

    Add model structs

---
+++ src/models/model.rs
@@ -0,0 +1,23 @@
+use crate::graphics::{Line, Triangle, Vertex};
+
+/// A model made of a line list.
+pub struct LineList {
+    pub primitive: Vec<Line<Vertex>>,
+}
+
+/// A model made of a line list with indexed vertices.
+pub struct IndexedLineList {
+    pub vertices: Vec<Vertex>,
+    pub primitives: Vec<Line<usize>>,
+}
+
+/// A model made of a triangle list.
+pub struct TriangleList {
+    pub primitives: Vec<Triangle<Vertex>>,
+}
+
+/// A model made of a triangle list with indexed vertices.
+pub struct IndexedTriangleList {
+    pub vertices: Vec<Vertex>,
+    pub primitives: Vec<Triangle<usize>>,
+}
