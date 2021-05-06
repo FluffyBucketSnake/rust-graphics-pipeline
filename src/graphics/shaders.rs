7bda8349c362816474e8651deb998f2b112710f1
FluffyBucketSnake <fluffybucketsnake@outlook.com>
2021-05-06 18:40:41 -0400

    Add BasicEffect

--- src/graphics/shaders.rs
+++ src/graphics/shaders.rs
@@ -1,3 +1,6 @@
+use cgmath::prelude::*;
+use cgmath::Matrix4;
+
 use crate::math::Color;
 
 use super::Vertex;
@@ -5,4 +8,35 @@ use super::Vertex;
 pub trait Effect {
     fn vs(&self, input: &Vertex) -> Vertex;
     fn ps(&self, input: &Vertex) -> Color;
+}
+
+
+pub struct BasicEffect {
+    pub worldviewproj: Matrix4<f32>,
+}
+
+impl BasicEffect {
+    pub fn new(worldviewproj: Matrix4<f32>) -> Self { 
+        Self { worldviewproj } 
+    }
+}
+
+impl Default for BasicEffect {
+    fn default() -> Self {
+        Self::new(Matrix4::identity())
+    }
+}
+
+impl Effect for BasicEffect {
+    fn vs(&self, input: &Vertex) -> Vertex {
+        let mut vertex = *input;
+
+        vertex.position = self.worldviewproj * vertex.position;
+
+        vertex
+    }
+
+    fn ps(&self, input: &Vertex) -> Color {
+        input.color
+    }
 }
