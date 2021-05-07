1c6a9c3e8e50a3473d1b1e65ca7410b367e6ac6f
FluffyBucketSnake <fluffybucketsnake@outlook.com>
2021-05-06 21:42:17 -0400

    Comment out unused functions

--- src/math/color.rs
+++ src/math/color.rs
@@ -24,25 +24,26 @@ pub fn vector4_to_color(vector: &Vector4<f32>) -> Color {
     )
 }
 
-/// Linearly interpolates two colors.
-pub fn mix(color1: Color, color2: Color, amount: f32) -> Color {
-    let (r1, g1, b1, a1) = color1.rgba();
-    let (r2, g2, b2, a2) = color2.rgba();
-    let amount = amount.max(0.0).min(1.0);
-    Color::RGBA(
-        lerp(r1, r2, amount),
-        lerp(g1, g2, amount),
-        lerp(b1, b2, amount),
-        lerp(a1, a2, amount),
-    )
-}
+// /// Linearly interpolates two colors.
+// pub fn mix(color1: Color, color2: Color, amount: f32) -> Color {
+//     let (r1, g1, b1, a1) = color1.rgba();
+//     let (r2, g2, b2, a2) = color2.rgba();
+//     let amount = amount.max(0.0).min(1.0);
+//     Color::RGBA(
+//         lerp(r1, r2, amount),
+//         lerp(g1, g2, amount),
+//         lerp(b1, b2, amount),
+//         lerp(a1, a2, amount),
+//     )
+// }
 
-/// Linear interpolation.
-#[inline]
-fn lerp(value1: u8, value2: u8, amount: f32) -> u8 {
-    (((value2 as f32) * amount) + ((value1 as f32) * (1.0 - amount))) as u8
-}
+// /// Linear interpolation.
+// #[inline]
+// fn lerp(value1: u8, value2: u8, amount: f32) -> u8 {
+//     (((value2 as f32) * amount) + ((value1 as f32) * (1.0 - amount))) as u8
+// }
 
+/// Float value clamping.
 #[inline]
 fn clamp(min: f32, value: f32, max: f32) -> f32 {
     f32::min(f32::max(value, min), max)
